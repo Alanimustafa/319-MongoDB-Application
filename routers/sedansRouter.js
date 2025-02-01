13400cff96ffcf17b6cdb1001f36d688d6eeb8d5
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-02-01 12:32:17 -0600

    Rendering the results from sedan inventory into the sedan index files in views/sedansViews

--- routers/sedansRouter.js
+++ routers/sedansRouter.js
@@ -8,9 +8,9 @@ const Sedan = require('../models/sedan.js'); // Requireing the Sedan model.
 sedansRouter.route("/")
 .get(async (req, res) => {
     const sedansInventory = await Sedan.find() // Finding all the sedans in the database.
-    console.log(sedansInventory);
-    res.send(sedansInventory) ;
-    // res.render("/views/sedans.ejs", { sedan: sedansInventory}) ;
+    // console.log(sedansInventory); // FOR TEST: Logging the sedans inventory.
+    res.render("./sedansViews/index.ejs", { sedan: sedansInventory}) ;
+    
 });
 
 module.exports = sedansRouter; // Exporting the sedans router.
